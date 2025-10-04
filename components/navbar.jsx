e5e162b257b2a368074e45566eeaa14e8c1a8b1d
steven <spha0034@student.monash.edu>
2025-10-04 18:57:53 +1000

    testing on live server

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -63,7 +63,7 @@ export default function Navbar() {
         <div className="flex items-center space-x-6">
             {/* Website Logo/ Name */}
             <Link href="/" className="text-xl font-bold text-white">
-                Blackjack
+                Blackjackssss
             </Link>
             
             {/* Buy more chips button */}
